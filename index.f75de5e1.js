8089b608c80c26179658a3f2ee6a8d1ef43756b6
TetianaBukhenko <tania04bukhenko@gmail.com>
2024-07-05 22:53:38 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const populationElements = document.querySelectorAll("span.population");
+const population = [
+    ...populationElements
+].map((el)=>+el.textContent.replaceAll(",", ""));
+const sum = population.reduce((prev, number)=>number + prev, 0);
+const totalPopulation = document.querySelectorAll("span.total-population")[0];
+const average = document.querySelectorAll("span.average-population")[0];
+totalPopulation.textContent = sum.toLocaleString("en-US");
+average.textContent = (sum / population.length).toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
